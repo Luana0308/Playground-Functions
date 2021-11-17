b3d6064d9a723a8c968922c3635e6f06312e9eb6
Luana <luanaff-2008@hotmail.com>
2021-11-17 15:34:30 -0300

    iniciando exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,7 +2,7 @@
 function techList(arrayList, name) {
   arrayList.sort()
   if(arrayList.length === 0){
-    return "Vazio!"
+    return "Vazio!" 
   }
   let arrayObjetc = []
   for(let i=0; i< arrayList.length; i+=1){
@@ -17,9 +17,18 @@ function techList(arrayList, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(numbertell){
+  if(numbertell.length !== 11){
+     return "Array com tamanho incorreto."
+  }
+  let phoneNumber = ""
+  for(i=0; i<numbertell.length; i+=1){
+    if(numbertell[i] > 9 || numbertell[i] < 0){
+      return "Não é possivel gerar um numero de telefone com esses valores"
+    }
+  }
+  return phoneNumber
+} console.log(generatePhoneNumber([-1,2,-3,4,5,11,1,1,1,2,1]))
 
 // Desafio 12
 function triangleCheck() {
